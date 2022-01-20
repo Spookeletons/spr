465bcaba20b7aa7fe98195ecc09d1e7fc9c14e92
Spookeletons <84mo@fresnou.org>
2022-01-20 08:55:39 -0800

    fixed, added some functions

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,29 @@
 const {Product} = require('../models')
+const categories = ['home goods', 'sports', 'appliances', 'pets', 'other'];
+
 module.exports.renderProfile = async function(req, res){
     const product = await Product.findByPk(
         req.params.id
     );
     res.render('products/profile', {product});
+}
+
+module.exports.renderEditForm = async function (req, res) {
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/edit', {product, categories});
+}
+
+module.exports.updateProduct = async function(req,res){
+    await Product.update({
+        product_name: req.body.product_name,
+        category: req.body.category,
+        description: req.body.description
+    }, {
+        where: {
+            id:req.params.id
+        }
+    });
+    res.redirect(`/products/profile/${req.params.id}`);
 }
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const productController = require('../controllers/productController')
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/products/profile:id', productController.renderProfile);
+router.get('/products/profile/:id', productController.renderProfile);
+router.get('/products/edit/:id', productController.renderEditForm);
 module.exports = router;
