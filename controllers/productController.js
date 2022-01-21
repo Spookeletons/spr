5bd55d38a33cbff8b0f1fa3cc0f21a5d31a45bec
Spookeletons <84mo@fresnou.org>
2022-01-21 08:56:08 -0800

    finished adding crud, added some more, but reviews don't work yet

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,9 @@ const categories = ['home goods', 'sports', 'appliances', 'pets', 'other'];
 
 module.exports.renderProfile = async function(req, res){
     const product = await Product.findByPk(
-        req.params.id
+        req.params.id, {
+            include:'reviews'
+        }
     );
     res.render('products/profile', {product});
 }
@@ -15,7 +17,7 @@ module.exports.renderEditForm = async function (req, res) {
     res.render('products/edit', {product, categories});
 }
 
-module.exports.updateProduct = async function(req,res){
+module.exports.updateProduct = async function(req, res){
     await Product.update({
         product_name: req.body.product_name,
         category: req.body.category,
@@ -26,4 +28,36 @@ module.exports.updateProduct = async function(req,res){
         }
     });
     res.redirect(`/products/profile/${req.params.id}`);
+}
+
+module.exports.viewProducts = async function(req, res){
+    const products = await Product.findAll();
+    res.render('index', {products})
+}
+
+module.exports.renderAddForm = function(req, res){
+    const product = {
+        product_name: '',
+        description: '',
+        category: categories[0]
+    };
+    res.render('products/add', {product, categories});
+}
+
+module.exports.addProduct = async function(req, res){
+    const result = await Product.create({
+        product_name: req.body.product_name,
+        category: req.body.category,
+        description: req.body.description
+    });
+    res.redirect(`/products/profile/${result.id}`)
+}
+
+module.exports.deleteProduct = async function(req, res){
+    await Product.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect('/products')
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController')
+const reviewController = require('../controllers/reviewController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/products', productController.viewProducts);
 router.get('/products/profile/:id', productController.renderProfile);
 router.get('/products/edit/:id', productController.renderEditForm);
+router.get('/products/add', productController.renderAddForm);
+router.post('/products/edit/:id', productController.updateProduct);
+router.get('/products/delete/:id', productController.deleteProduct);
+router.post('/products/add', productController.addProduct);
+router.get('/products/:productId/review/add', reviewController.renderAddReviewForm);
 module.exports = router;
